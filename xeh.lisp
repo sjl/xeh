9c92311efa086ef460a0a9ddd3ffc768cfbcf35c
Steve Losh <steve.losh@10xgenomics.com>
2021-06-23 21:19:26 -0400

    Slim things down a bit

--- xeh.lisp
+++ xeh.lisp
@@ -1,108 +1,38 @@
 ;;; xeh
-;;; A command-line Canonical HEX+ASCII display tool written in ~100 lines of Common Lisp.
+;;; A command-line Canonical HEX+ASCII display tool written in ~40 lines of Common Lisp.
 ;;; https://github.com/obsfx/xeh
 
-(defun dump (output) 
-  (format t "~A" output))
+(defun slice (sequence start end &aux (l (length sequence)))
+  (coerce (subseq sequence (min start l) (min end l)) 'list))
 
-(defun dump-n (output n) 
-  ;; https://stackoverflow.com/a/24758778/13615958
-  (format t "~v@{~A~:*~}" n output))
+(defun hex-to-str-char (i)
+  (if (< 31 i 127)
+    (code-char i)
+    #\.))
 
-(defun repeat-n (output n) 
-  ;; https://stackoverflow.com/a/24758778/13615958
-  (format nil "~v@{~A~:*~}" n output))
+(defun dump-hex-row (buffer addr)
+  (format t "~(~8,'0X  ~{~2,'0X~^ ~}  ~{~2,'0X~^ ~}~) ~60T|~A|~%"
+          addr
+          (slice buffer 0 8)
+          (slice buffer 8 16)
+          (map 'string #'hex-to-str-char buffer)))
 
-(defun newline () 
-  (format t "~%"))
+(defun read-file-bytes (stream &aux (addr 0) (buffer (make-array 16 :fill-pointer 0)))
+  (loop :for byte = (read-byte stream nil nil)
+        :while byte
+        :do (progn (vector-push byte buffer)
+                   (when (= (length buffer) 16)
+                     (dump-hex-row buffer addr)
+                     (setf (fill-pointer buffer) 0
+                           addr (+ addr 16)))))
+  (when (plusp (length buffer))
+    (dump-hex-row buffer addr))
+  (format t "~(~8,'0X~)~%" (+ addr (length buffer))))
 
-(defun slice (l start len)
-  (if (< start (length l))
-      (let ((end (+ start len)))
-        (subseq l start (if (< end (length l)) end (length l))))))
-
-(defun fixed-slice (l start len)
-  (let ((base-list (make-list len :initial-element "  ")))
-    (loop for i from 0 to (- len 1)
-          do (let* ((hex (nth (+ start i) l))
-                    (placed-val (if hex hex "  ")))
-               (setf (nth i base-list) placed-val)))
-    (values base-list)))
-
-(defun join (str-list &optional char)
-  (reduce 
-    #'(lambda (a b)
-        (format nil "~A~A~A" a (if char char "") b))
-    str-list))
-
-(defun format-single-char-hex-values (hex-list)
-  (mapcar 
-    #'(lambda (hex)
-        (format nil "~A~A" (repeat-n "0" (- 2 (length hex))) hex))
-    hex-list))
-
-(defun get-row-address (byte-pos)
-  (let ((hex-byte-pos (write-to-string byte-pos :base 16)))
-    (format nil "~A~A" (repeat-n "0" (- 8 (length hex-byte-pos))) hex-byte-pos)))
-
-(defun hex-list-to-str-list (str-list)
-  (mapcar 
-    #'(lambda (str) 
-        (let* ((ascii (parse-integer str :radix 16))
-               (c (code-char ascii)))
-          (if (and c (> ascii 31) (< ascii 127)) c (format nil "."))))
-    str-list))
-
-(defun dump-hex-row (buffer byte-pos)
-  (let* ((row (fixed-slice buffer 0 16))
-         (left (subseq row 0 8))
-         (right (subseq row 8 16)))
-    (progn
-      (dump (string-downcase (get-row-address byte-pos))) (dump "  ")
-      (dump (string-downcase (join left " "))) (dump "  ")
-      (dump (string-downcase (join right " "))) (dump "  ")
-      (dump "|")
-      (dump (join (hex-list-to-str-list buffer)))
-      (dump "|")
-      (newline))))
-
-(defparameter byte-pos 0)
-(defparameter buffer nil)
-(defparameter file-path nil)
-(defun read-file-bytes (file-path)
-  (with-open-file 
-      (stream 
-        file-path
-        :element-type '(unsigned-byte 8)
-        :if-does-not-exist nil)
-    ;; https://stackoverflow.com/a/3814098/13615958
-    ;; https://lispcookbook.github.io/cl-cookbook/iteration.html
-    (if stream
-        (progn
-          (loop named stream-loop 
-                for byte = (read-byte stream nil nil)
-                while byte
-                do (progn
-                     (setq buffer (append buffer (list (write-to-string byte :base 16))))
-                     (if (= (length buffer) 16)
-                         (progn
-                           (setf buffer (format-single-char-hex-values buffer))
-                           (dump-hex-row buffer byte-pos)
-                           (setf byte-pos (+ byte-pos 16))
-                           (setf buffer nil)))))
-          (if (> (length buffer) 0)
-              (progn
-                (setf buffer (format-single-char-hex-values buffer))
-                (dump-hex-row buffer byte-pos)))
-          (dump (string-downcase (get-row-address (+ byte-pos (length buffer)))))
-          (newline))
-        (progn
-          (format t "xeh: ~A: File couldn't found" file-path)
-          (newline)
-          (quit)))))
-
-(defun main()
-    (progn
-      (setf file-path (second *posix-argv*))
-      (if file-path 
-          (read-file-bytes file-path))))
+(defun main ()
+  (sb-ext:disable-debugger)
+  (dolist (path (or (rest *posix-argv*) '("-")))
+    (if (string= path "-")
+      (read-file-bytes *standard-input*)
+      (with-open-file (stream path :element-type '(unsigned-byte 8))
+        (read-file-bytes stream)))))
